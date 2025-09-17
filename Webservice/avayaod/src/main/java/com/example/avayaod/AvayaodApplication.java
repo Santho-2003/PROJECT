fa4ef2124620b822ecbd252d0de9f05b0184ab92
Santhoshini.B <117194680+Santho-2003@users.noreply.github.com>
2025-09-17 15:29:23 +0530

    Add files via upload

---
+++ Webservice/avayaod/src/main/java/com/example/avayaod/AvayaodApplication.java
@@ -0,0 +1,13 @@
+package com.example.avayaod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvayaodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvayaodApplication.class, args);
+	}
+
+}
---
+++ Webservice/avayaod/src/test/java/com/example/avayaod/AvayaodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avayaod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvayaodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
